c840d4709e7b1d5a0673d2bbc302363a32435901
AnthoniBortolotto <antho@outlook.com.br>
2025-03-07 21:02:27 -0300

    Project - migrated project from Javascript to Typescript

---
+++ src/App.tsx
@@ -0,0 +1,38 @@
+import { useEffect, useState } from "react";
+import "./App.css";
+
+function App() {
+  const [demos, setDemos] = useState<any>();
+
+  useEffect(() => {
+    const fetchData = async () => {
+      const response = await fetch("http://localhost:3001/demos", {
+        method: "GET",
+      });
+      setDemos(await response.json());
+    };
+
+    fetchData();
+  }, []);
+
+  return (
+    <div
+      style={{
+        width: "100%",
+        height: "100%",
+        padding: "2rem",
+        boxSizing: "border-box",
+      }}
+    >
+      {demos &&
+        demos.map((demo: any) => (
+          <div key={demo.id} className="card">
+            <h2>{demo.name}</h2>
+            <button>Visualizar</button>
+          </div>
+        ))}
+    </div>
+  );
+}
+
+export default App;
